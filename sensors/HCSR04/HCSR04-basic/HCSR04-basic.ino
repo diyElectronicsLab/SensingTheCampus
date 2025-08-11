8832b421f26cc7e201f5a0886fba9a6c2e49bb3f
Christian Doeller <mail@christiandoeller.de>
2025-08-11 20:23:07 +0200

    added hcr code

---
+++ sensors/HCSR04/HCSR04-basic/HCSR04-basic.ino
@@ -0,0 +1,28 @@
+#define TRIG_PIN 39
+#define ECHO_PIN 36
+
+#define SOUND_SPEED 0.034
+
+long duration;
+float distanceCm;
+
+void setup() {
+  Serial.begin(9600);        // Starts the serial communication
+  pinMode(TRIG_PIN, OUTPUT);  // Sets the trigPin as an Output
+  pinMode(ECHO_PIN, INPUT);   // Sets the echoPin as an Input
+}
+
+void loop() {
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  distanceCm = duration * SOUND_SPEED / 2;
+
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+
+  delay(500);
+}
