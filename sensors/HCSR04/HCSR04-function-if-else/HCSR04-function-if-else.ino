fee06425411c7da93831b3b13ef93b5124afd51c
diyElectronicsLab <christian.doeller@uni-weimar.de>
2025-11-06 17:10:22 +0100

    added if else function

---
+++ sensors/HCSR04/HCSR04-function-if-else/HCSR04-function-if-else.ino
@@ -0,0 +1,38 @@
+#define TRIG_PIN 22
+#define ECHO_PIN 23
+
+#define SOUND_SPEED 0.034
+
+long duration;
+float distanceCm;
+
+void setup() {
+  Serial.begin(9600);        // Starts the serial communication
+  pinMode(TRIG_PIN, OUTPUT);  // Sets the trigPin as an Output
+  pinMode(ECHO_PIN, INPUT);   // Sets the echoPin as an Input
+}
+
+void loop() {
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  distanceCm = duration * SOUND_SPEED / 2;
+
+  if (distanceCm > 350 || distanceCm < 3) {
+        Serial.println("Distance out of range.");
+        // and, for example, turn off LED
+  } else{
+    if (distanceCm >= 100 && distanceCm <= 200){
+        // do something, for example: turn on LED
+    } else {
+        Serial.print("Distance (cm): ");
+        Serial.println(distanceCm);
+        // and, for example, turn off LED
+    }
+  }
+
+  delay(500);
+}
--- sensors/HCSR04/HCSR04-if-else/HCSR04-if-else.ino
+++ sensors/HCSR04/HCSR04-if-else/HCSR04-if-else.ino
@@ -1,5 +1,6 @@
 #define TRIG_PIN 22
 #define ECHO_PIN 23
+#define LED_PIN 2
 
 #define SOUND_SPEED 0.034
 
@@ -10,27 +11,46 @@ void setup() {
   Serial.begin(9600);        // Starts the serial communication
   pinMode(TRIG_PIN, OUTPUT);  // Sets the trigPin as an Output
   pinMode(ECHO_PIN, INPUT);   // Sets the echoPin as an Input
+  pinMode(LED_PIN, OUTPUT);  // Sets the LED_PIN as an Output
 }
 
 void loop() {
+  // call the measrueCM() function and save distance in a variable
+  float distance = measureCM();
+  // call the checkLED(float _distance, float _threshold) function
+  checkLED(distance, 100);
+
+  delay(500);
+}
+
+// === Function: measuring CM ===
+// returns measured CM as type float
+float measureCM() {
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
   duration = pulseIn(ECHO_PIN, HIGH);
-
   distanceCm = duration * SOUND_SPEED / 2;
 
-  if (distanceCm > 350 || distanceCm < 3) {
-      Serial.print("Distance out of range.");
-      // and, for example, turn off LED
-  } else if (distanceCm >= 100 && distanceCm <= 200) {
-      // do something, for example: turn on LED
+  return distanceCm;
+}
+
+// === Function: checking LED ===
+// check if LED should light up with a certain threshold value
+void checkLED(float _distance, float _threshold) {
+  if (_distance > 350 || _distance < 3) {
+      Serial.println("Distance out of range.");
+      digitalWrite (LED_PIN, LOW);
   } else {
       Serial.print("Distance (cm): ");
-      Serial.println(distanceCm);
-      // and, for example, turn off LED
+      Serial.println(_distance);
+      if (_distance >= _threshold) {
+          Serial.println("LED is turned on.");
+          digitalWrite (LED_PIN, HIGH);
+      } else {
+          Serial.println("LED is turned off.");
+          digitalWrite (LED_PIN, LOW);
+      }
   }
-
-  delay(500);
 }
