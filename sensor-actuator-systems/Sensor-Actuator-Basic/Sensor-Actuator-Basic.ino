34588524f5f09015079aeb9f5f8ff2c00e3051e3
diyElectronicsLab <christian.doeller@uni-weimar.de>
2025-11-12 18:52:05 +0100

    Update Sensor-Actuator-Basic.ino

--- sensor-actuator-systems/Sensor-Actuator-Basic/Sensor-Actuator-Basic.ino
+++ sensor-actuator-systems/Sensor-Actuator-Basic/Sensor-Actuator-Basic.ino
@@ -9,11 +9,8 @@ Servo myServo;
 #define ECHO_PIN 23
 
 #define SOUND_SPEED 0.034
-#define MAX_CM 30  // maximum (measurement range)
-#define MIN_CM 0   // minimum (measurement range)
-
-long duration;
-float distanceCm;
+#define MAX_CM 100  // maximum (measurement range)
+#define MIN_CM 5    // minimum (measurement range)
 
 
 // === setup ===
@@ -35,22 +32,35 @@ void loop() {
   // Sensor function call
   // return value from measurement and save it in distance variable
   float distance = measureCM();
+  Serial.print("measured distance: ");
+  Serial.print(distance);
 
-  // constrain the distance to our wanted range
-  // constrain (value, minimum, maximum)
-  float constrainedDistance = constrain(distance, MIN_CM, MAX_CM);
-  Serial.print("constrained value: ");
-  Serial.println(constrainedDistance);
+  if (distance > MIN_CM) {
+    // constrain the distance to our wanted range
+    // constrain (value, minimum, maximum)
+    float constrainedDistance = constrain(distance, MIN_CM, MAX_CM);
+    Serial.print('\t');
+    Serial.print("constrained distance: ");
+    Serial.print(constrainedDistance);
 
-  // map the sensor value according to our range and the servo angles
-  // map(value, from minimum, from maximum, to minimum, to maximum)
-  float mappedDistance = map(constrainedDistance, MIN_CM, MAX_CM, 180, 0);
-  Serial.print("mapped value: ");
-  Serial.println(mappedDistance);
+    // float Mapping formula instead of map() function
+    float angle = constrainedDistance * 180.0 / (MAX_CM - MIN_CM);
+    Serial.print('\t');
+    Serial.print("angle: ");
+    Serial.print(angle);
 
-  // drive the servo motor according to the sensor value
-  myServo.write(mappedDistance);
-  delay (50); // pause for servo to react
+    // drive the servo motor according to the sensor value
+    // if difference between current and future smaller than 1, do not move 
+    // > prevent servo from stuttering (optional)
+    float currentAngle = myServo.read();
+
+    if (abs(currentAngle - angle) > 1) {
+      myServo.write(angle);
+    }
+  }
+
+  Serial.println();
+  delay(50);
 }
 
 
@@ -61,8 +71,9 @@ float measureCM() {
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distanceCm = duration * SOUND_SPEED / 2;
+  long duration = pulseIn(ECHO_PIN, HIGH, 20000); // 30 ms = max ~5m timeout
+  if (duration == 0) return 0; // timeout: return 0
 
+  float distanceCm = duration * SOUND_SPEED / 2;
   return distanceCm;
 }
