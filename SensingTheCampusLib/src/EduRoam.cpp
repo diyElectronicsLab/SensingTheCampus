c94489832a80b13eec80e5e61422aa41c8457851
diyElectronicsLab <christian.doeller@uni-weimar.de>
2026-02-01 13:08:03 +0100

    non blocking eduroam CALL IN LOOP NECESSARY

--- SensingTheCampusLib/src/EduRoam.cpp
+++ SensingTheCampusLib/src/EduRoam.cpp
@@ -2,23 +2,41 @@
 #include <WiFi.h>
 #include "esp_wpa2.h"
 
-void connectToEduRoam(String userName, String pwd) {
-  if (WiFi.status() == WL_CONNECTED) return;
-  
-  const char* ssid = "eduroam";
+static bool wifiConnecting = false;
+static unsigned long lastAttempt = 0;
+const unsigned long attemptInterval = 2000; // alle 2 Sekunden prüfen
+
+void connectToEduRoam(String userName, String pwd)
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    wifiConnecting = false;
+    return;
+  }
+
+  const char *ssid = "eduroam";
   String EAP_ANONYMOUS_IDENTITY = userName;
 
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_ANONYMOUS_IDENTITY, userName, pwd);
-
-  Serial.print("Connecting to eduroam");
-  while (WiFi.status() != WL_CONNECTED) {  // if not yet connected, try again and print a "."
-    delay(500);
-    Serial.print(".");
+  if (!wifiConnecting)
+  {
+    WiFi.disconnect(true);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_ANONYMOUS_IDENTITY, userName, pwd);
+    wifiConnecting = true;
+    lastAttempt = millis();
+    Serial.print("Starting WiFi connection...");
+    return; // more cennection tries in main loop
   }
-  Serial.println("\nWiFi connected. IP: ");
-  Serial.println(WiFi.localIP());  // show the IP of device in serial monitor
 
-  Serial.println();  // print end of line
+  // connecting, check and try again every 2 seconds 
+  if (millis() - lastAttempt > attemptInterval)
+  {
+    lastAttempt = millis();
+    Serial.print(".");
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      Serial.println("\nWiFi connected! IP: " + WiFi.localIP().toString());
+      wifiConnecting = false;
+    }
+  }
 }
